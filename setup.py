f632be113342147a4ed2066ac1c72d9265b8a099
Michael <michael@lootbox.WORKGROUP>
2021-02-01 12:29:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@ from setuptools import setup
 
 setup(
     name='Render Watch',
-    version='0.1',
+    version='0.1.0',
     packages=['ffmpeg', 'startup', 'encoding', 'app_handlers', 'app_formatting'],
     package_dir={'': 'src'},
+    py_modules=['main'],
+    install_requires=[
+        'PyGObject',
+        'watchdog',
+    ],
+    python_requires='>=3.8',
     url='https://github.com/mgregory1994/RenderWatch',
     license='GPLV3',
     author='Michael Gregory',
