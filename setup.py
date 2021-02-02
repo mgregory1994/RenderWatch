7503a48a84866b40cc71711de11ec6df49141769
Michael Gregory <michaelgregory@csus.edu>
2021-02-01 16:02:29 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Render Watch',
     version='0.1.0',
-    packages=['ffmpeg', 'startup', 'encoding', 'app_handlers', 'app_formatting'],
+    packages=['startup', 'ffmpeg', 'encoding', 'app_handlers', 'app_formatting', 'render_watch_data'],
     package_dir={'': 'src'},
     py_modules=['main'],
     install_requires=[
         'PyGObject',
         'watchdog',
     ],
+    package_data={
+        'render_watch_data': ['*']
+    },
     python_requires='>=3.8',
     url='https://github.com/mgregory1994/RenderWatch',
     license='GPLV3',
