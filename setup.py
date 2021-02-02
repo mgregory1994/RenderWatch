0f071a8f105911f5a366dfcc485876b30c817cc1
Michael Gregory <michaelgregory@csus.edu>
2021-02-01 18:32:47 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,27 @@ from setuptools import setup, find_packages
 setup(
     name='Render Watch',
     version='0.1.0',
-    packages=['startup', 'ffmpeg', 'encoding', 'app_handlers', 'app_formatting', 'render_watch_data'],
+    url='https://github.com/mgregory1994/RenderWatch',
+    license='GPLV3',
+    author='Michael Gregory',
+    author_email='michaelgregory@csus.edu',
+    description='A professional video transcoder for Linux.',
+    python_requires='>=3.8',
     package_dir={'': 'src'},
-    py_modules=['main'],
+    packages=['render_watch', 'render_watch/app_formatting', 'render_watch/app_handlers', 'render_watch/encoding',
+              'render_watch/ffmpeg', 'render_watch/render_watch_data', 'render_watch/startup'],
     install_requires=[
         'PyGObject',
         'watchdog',
     ],
     package_data={
-        'render_watch_data': ['*']
+        'render_watch/render_watch_data': ['*']
     },
-    python_requires='>=3.8',
-    url='https://github.com/mgregory1994/RenderWatch',
-    license='GPLV3',
-    author='Michael Gregory',
-    author_email='michaelgregory@csus.edu',
-    description='A professional video transcoder for Linux.'
+    data_files=[
+        ('share/applications', ['data/render-watch.desktop']),
+        ('share/icons/hicolor/64x64/apps', ['data/icons/hicolor/64x64/apps/RenderWatch.png']),
+        ('share/icons/hicolor/128x128/apps', ['data/icons/hicolor/128x128/apps/RenderWatch.png']),
+        ('share/icons/hicolor/512x512/apps', ['data/icons/hicolor/512x512/apps/RenderWatch.png'])
+    ],
+    scripts=['scripts/render-watch', 'scripts/render-watch-debug']
 )
