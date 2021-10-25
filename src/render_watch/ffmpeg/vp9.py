edbde8b425a57230a5e6d497c3bfe6a54fa42c27
Michael Gregory <michaelgregoryn@gmail.com>
2021-10-24 21:10:41 -0600

    Updated coding style: vp9.py

--- src/render_watch/ffmpeg/vp9.py
+++ src/render_watch/ffmpeg/vp9.py
@@ -17,7 +17,9 @@
 
 
 class VP9:
-    """Manages all settings for the VP9 codec."""
+    """
+    Stores all settings for the VP9 codec.
+    """
 
     QUALITY_ARGS_LIST = ('auto', 'good', 'best', 'realtime')
     QUALITY_LIST_LENGTH = len(QUALITY_ARGS_LIST)
@@ -36,7 +38,9 @@ class VP9:
 
     @property
     def quality(self):
-        """Returns quality argument as an index."""
+        """
+        Returns quality as an index.
+        """
         if '-deadline' in self.ffmpeg_args:
             quality_arg = self.ffmpeg_args['-deadline']
             return self.QUALITY_ARGS_LIST.index(quality_arg)
@@ -44,7 +48,9 @@ class VP9:
 
     @quality.setter
     def quality(self, quality_index):
-        """Stores index as a quality argument."""
+        """
+        Stores quality index as a string.
+        """
         if quality_index is None or not 0 < quality_index < VP9.QUALITY_LIST_LENGTH:
             self.ffmpeg_args.pop('-deadline', 0)
         else:
@@ -52,7 +58,9 @@ class VP9:
 
     @property
     def speed(self):
-        """Returns speed argument as an index."""
+        """
+        Returns speed as an index.
+        """
         if '-cpu-used' in self.ffmpeg_args:
             speed_arg = self.ffmpeg_args['-cpu-used']
             return self.SPEED_ARGS_LIST.index(speed_arg)
@@ -60,7 +68,9 @@ class VP9:
 
     @speed.setter
     def speed(self, speed_index):
-        """Stores index as a speed argument."""
+        """
+        Stores speed index as a string.
+        """
         if speed_index is None or not 0 < speed_index < VP9.SPEED_LIST_LENGTH:
             self.ffmpeg_args.pop('-cpu-used', 0)
         else:
@@ -68,90 +78,114 @@ class VP9:
 
     @property
     def bitrate(self):
-        """Returns bitrate argument as an int."""
+        """
+        Returns bitrate as an int.
+        """
         bitrate_arg = self.ffmpeg_args['-b:v']
         return int(bitrate_arg.split('k')[0])
 
     @bitrate.setter
-    def bitrate(self, bitrate_value):
-        """Stores bitrate value as a string argument."""
-        if bitrate_value is None or not 0 <= bitrate_value <= 99999:
+    def bitrate(self, bitrate):
+        """
+        Stores bitrate as a string.
+        """
+        if bitrate is None or not 0 <= bitrate <= 99999:
             self.ffmpeg_args['-b:v'] = '2500k'
         else:
-            self.ffmpeg_args['-b:v'] = str(bitrate_value) + 'k'
+            self.ffmpeg_args['-b:v'] = str(bitrate) + 'k'
 
     @property
     def crf(self):
-        """Returns crf argument as a float."""
+        """
+        Returns crf as a float.
+        """
         if '-crf' in self.ffmpeg_args:
             return float(self.ffmpeg_args['-crf'])
         return None
 
     @crf.setter
-    def crf(self, crf_value):
-        """Stores crf value as a string argument."""
-        if crf_value is None or not 0 <= crf_value <= 63:
+    def crf(self, crf):
+        """
+        Stores crf as a string.
+        """
+        if crf is None or not 0 <= crf <= 63:
             self.ffmpeg_args.pop('-crf', 0)
         else:
-            self.ffmpeg_args['-crf'] = str(crf_value)
+            self.ffmpeg_args['-crf'] = str(crf)
 
     @property
     def maxrate(self):
-        """Returns maxrate argument as an int."""
+        """
+        Returns maxrate as an int.
+        """
         if '-maxrate' in self.ffmpeg_args:
             maxrate_arg = self.ffmpeg_args['-maxrate']
             return int(maxrate_arg.split('k')[0])
         return None
 
     @maxrate.setter
-    def maxrate(self, maxrate_value):
-        """Stores maxrate value as a string argument."""
-        if maxrate_value is None or not 0 <= maxrate_value <= 99999:
+    def maxrate(self, maxrate):
+        """
+        Stores maxrate as a string.
+        """
+        if maxrate is None or not 0 <= maxrate <= 99999:
             self.ffmpeg_args.pop('-maxrate', 0)
         else:
-            self.ffmpeg_args['-maxrate'] = str(maxrate_value) + 'k'
+            self.ffmpeg_args['-maxrate'] = str(maxrate) + 'k'
 
     @property
     def minrate(self):
-        """Returns minrate argument as an int."""
+        """
+        Returns minrate as an int.
+        """
         if '-minrate' in self.ffmpeg_args:
             minrate_arg = self.ffmpeg_args['-minrate']
             return int(minrate_arg.split('k')[0])
         return None
 
     @minrate.setter
-    def minrate(self, minrate_value):
-        """Stores minrate value as a string argument."""
-        if minrate_value is None or not 0 <= minrate_value <= 99999:
+    def minrate(self, minrate):
+        """
+        Stores minrate as a string.
+        """
+        if minrate is None or not 0 <= minrate <= 99999:
             self.ffmpeg_args.pop('-minrate', 0)
         else:
-            self.ffmpeg_args['-minrate'] = str(minrate_value) + 'k'
+            self.ffmpeg_args['-minrate'] = str(minrate) + 'k'
 
     @property
     def encode_pass(self):
-        """Returns encode pass argument as an int."""
+        """
+        Returns encode pass as an int.
+        """
         if '-pass' in self.ffmpeg_args:
             return int(self.ffmpeg_args['-pass'])
         return None
 
     @encode_pass.setter
-    def encode_pass(self, encode_pass_value):
-        """Stores encode pass value as a string argument."""
-        if encode_pass_value is None or not 1 <= encode_pass_value <= 2:
+    def encode_pass(self, encode_pass):
+        """
+        Stores encode pass as a string.
+        """
+        if encode_pass is None or not 1 <= encode_pass <= 2:
             self.ffmpeg_args.pop('-pass', 0)
         else:
-            self.ffmpeg_args['-pass'] = str(encode_pass_value)
+            self.ffmpeg_args['-pass'] = str(encode_pass)
 
     @property
     def stats(self):
-        """Returns pass log file argument as a string."""
+        """
+        Returns pass log file path as a string.
+        """
         if '-passlogfile' in self.ffmpeg_args:
             return self.ffmpeg_args['-passlogfile']
         return None
 
     @stats.setter
     def stats(self, stats_file_path):
-        """Stores pass log file path as a string argument."""
+        """
+        Stores pass log file path as a string.
+        """
         if stats_file_path is None:
             self.ffmpeg_args.pop('-passlogfile', 0)
         else:
@@ -159,21 +193,27 @@ class VP9:
 
     @property
     def row_multithreading(self):
-        """Returns row multithreading argument as a boolean."""
+        """
+        Returns row multithreading as a boolean.
+        """
         if '-row-mt' in self.ffmpeg_args:
             row_multithreading_arg = self.ffmpeg_args['-row-mt']
             return row_multithreading_arg == '1'
         return False
 
     @row_multithreading.setter
-    def row_multithreading(self, row_multithreading_value):
-        """Stores row multithreading boolean as a string argument."""
-        if row_multithreading_value is None or not row_multithreading_value:
+    def row_multithreading(self, row_multithreading_enabled):
+        """
+        Stores row multithreading as a string argument.
+        """
+        if row_multithreading_enabled is None or not row_multithreading_enabled:
             self.ffmpeg_args.pop('-row-mt', 0)
         else:
             self.ffmpeg_args['-row-mt'] = '1'
 
     @staticmethod
     def get_ffmpeg_advanced_args():
-        """Returns null advanced settings dictionary."""
+        """
+        Null dictionary for compatibility.
+        """
         return {'': None}
