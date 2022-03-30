d0879c755fa8c07d3a77dd8a459828fd5f00dc5a
Michael Gregory <michaelgregoryn@gmail.com>
2022-03-29 19:56:39 -0600

    Added doc strings to trim.py.

--- src/render_watch/ffmpeg/trim.py
+++ src/render_watch/ffmpeg/trim.py
@@ -17,31 +17,64 @@
 
 
 class TrimSettings:
+    """Class that configures all trim settings available for Render Watch."""
+
     def __init__(self):
+        """Initializes the TrimSettings class with all necessary variables for the trim options."""
         self.ffmpeg_args = {}
 
     @property
     def start_time(self) -> float | None:
+        """
+        Returns the trim start time.
+
+        Returns:
+            Trim start time as a float in seconds.
+        """
         if '-ss' in self.ffmpeg_args:
             return float(self.ffmpeg_args['-ss'])
         return None
 
     @start_time.setter
-    def start_time(self, start_time_in_seconds: int | None):
+    def start_time(self, start_time_in_seconds: float | None):
+        """
+        Sets the trim start time.
+
+        Parameters:
+            start_time_in_seconds: Trim segment start time in seconds.
+
+        Returns:
+            None
+        """
         if start_time_in_seconds:
-            self.ffmpeg_args['-ss'] = str(start_time_in_seconds)
+            self.ffmpeg_args['-ss'] = str(round(start_time_in_seconds, 2))
         else:
             self.ffmpeg_args.pop('-ss', 0)
 
     @property
     def trim_duration(self) -> float | None:
+        """
+        Returns the trim duration.
+
+        Returns:
+            Trim duration as a float in seconds.
+        """
         if '-to' in self.ffmpeg_args:
             return float(self.ffmpeg_args['-to'])
         return None
 
     @trim_duration.setter
-    def trim_duration(self, trim_duration_in_seconds):
+    def trim_duration(self, trim_duration_in_seconds: float | None):
+        """
+        Sets the trim duration.
+
+        Parameters:
+            trim_duration_in_seconds: Duration of the trim segment in seconds.
+
+        Returns:
+            None
+        """
         if trim_duration_in_seconds is None:
             self.ffmpeg_args.pop('-to', 0)
         else:
-            self.ffmpeg_args['-to'] = str(trim_duration_in_seconds)
+            self.ffmpeg_args['-to'] = str(round(trim_duration_in_seconds, 2))
